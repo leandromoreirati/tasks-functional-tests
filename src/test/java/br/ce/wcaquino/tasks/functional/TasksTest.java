e466342062c73b58c219c8be2531357022123cd4
Leandro Moreira <30784301+leandromoreirati@users.noreply.github.com>
2020-08-02 15:42:30 -0300

    fix webdriver

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -14,9 +14,9 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class TasksTest {
 	
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-//		WebDriver driver = new ChromeDriver();
+ 		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), cap);
+		//WebDriver driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), cap);
 		driver.navigate().to("http://tomcat:8000/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
